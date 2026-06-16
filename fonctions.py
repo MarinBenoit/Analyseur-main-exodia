055c9160c83acff7e6380f53890651fa1dd771c0
MarinBenoit <141397353+MarinBenoit@users.noreply.github.com>
2026-06-16 13:05:53 +0200

    Update fonctions.py
    Ajout fonction compteur de cartes

--- fonctions.py
+++ fonctions.py
@@ -21,7 +21,7 @@ def analyse_main_type(main):
     return NbMonstre, NbMagie, NbPiege, NbExodia
 # Fin de la définition de ma fonction, elle renvoie le nombre de monstre, piège et magie dans la main tirer
 
-def analyse_score_exodia(main, score_exodia):
+def analyse_score_exodia(main):
     Exodia = 0
     
     for carte in main:
@@ -33,3 +33,12 @@ def analyse_score_exodia(main, score_exodia):
             
     return Exodia
 # Fin de la définition de ma fonction, elle renvoie le score de ma main pour réussir à récuperer exodia
+
+def apparition_cartes(main,Compteurs_Cartes_exodia):
+    for carte in main:
+        if carte["nom"] in Compteurs_Cartes_exodia:
+            Compteurs_Cartes_exodia[carte["nom"]] += 1
+            
+        else:
+            Compteurs_Cartes_exodia[carte["nom"]] = 1
+# Fin de la définition de la fonction de compteur de carte
